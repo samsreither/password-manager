5e13262a3b7925dbdb45d73901fb32d6493c9fc4
Sam Reither <samsreither@gmail.com>
2025-01-27 09:32:11 -0500

    edits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="adding-password">
+        <input type="text" placeholder="Ex. password123" onChange={(event) => {setPassword(event.target.value)}}/>
+        <input type="text" placeholder="Ex. password123" />
+        <button> Add Password </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
+const mysql = require('mysql');
 const PORT = 3001
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'PasswordManager'
+})
 
 
 app.get('/', (req, res) => {
